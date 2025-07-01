bce440611bca08042cbb59a4252f2f2652bb1f61
Sakib Khan <sakibhkhan11@gmail.com>
2025-07-01 00:49:36 -0400

    changed reload to top page fix

--- scripts.js
+++ scripts.js
@@ -1,11 +1,3 @@
-// always start at top when refreshing
-if ('scrollRestoration' in history) {
-  history.scrollRestoration = 'manual';
-}
-window.addEventListener('load', () => {
-  window.scrollTo(0, 0);
-});
-
 // --- Work Experience ---
 const experienceContent = {
   persado: {
